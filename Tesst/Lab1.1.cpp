e8884f9a729a23436a9d679e9f4e0dab9080074e
ToHuuDang <tohuudang14@gmail.com>
2023-02-20 18:40:32 +0700

    update

---
+++ Tesst/Lab1.1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+double distance_traveled(double speed, double time) {
+    return speed * time;
+}
+
+void print_table(double speed, double time) {
+    cout << "Hour\tDistance Traveled\n";
+
+    for (int i = 1; i <= time; i++) {
+        double distance = distance_traveled(speed, i);
+       cout << i << "\t" << distance << "\n";
+    }
+}
+
+int main() {
+    double speed, time;
+
+    cout << "Nhap vao toc do:  ";
+    cin >> speed;
+
+    cout << "Nhap vao thoi gian: ";
+    cin >> time;
+
+    print_table(speed, time);
+
+    return 0;
+}
